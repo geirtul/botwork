6ee64f9c27fb287d8e4ec50e0d9e9a6b1be9e683
Geir Ulvik <ulvik@usit.uio.no>
2017-12-27 23:04:35 +0100

    Expanded with serve_forever and related functions.

--- annabot.py
+++ annabot.py
@@ -120,4 +120,121 @@ class AnnaBot(object):
             pres.setTag('x', namespace=NS_MUC).setTagData('password', password)
         self.connect().send(pres)
 
+    ## MUC END
+    def get_sender_username(self, mess):
+        """Extract the sender's user name from a message"""
+        type = mess.getType()
+        jid = mess.getFrom()
+        if type == "groupchat":
+            username = jid.getResource()
+        elif type == "chat":
+            username = jid.getNode()
+        else:
+            username = ""
+        return username
+
+
+
+    def callback_message(self, conn, mess):
+        """Messages sent to the bot will arrive here.
+        Command handling + routing is done in this function."""
+
+        # Prepare to handle only conference type messages.
+        type = mess.getType()
+        jid = mess.getFrom()
+        props = mess.getProperties()
+        text = mess.getBody()
+        username = self.get_sender_username(mess)
+
+        if type not "conference":
+            self.log.debug("unhandled message type: %s" % type)
+            return
+
+        # Ignore messages from before we joined
+        if xmpp.NS_DELAY in props:
+            return
+
+        # Ignore messages from myself
+        if self.jid.bareMatch(jid):
+            return
+
+        self.log.debug("*** props = %s" % props)
+        self.log.debug("*** jid = %s" % jid)
+        self.log.debug("*** username = %s" % username)
+        self.log.debug("*** type = %s" % type)
+        self.log.debug("*** text = %s" % text)
+
+        # If a message format is not supported (eg. encrypted),
+        # txt will be None
+        if not text:
+            return
+
+        # Match regex and perform RT API call here
+        # Needs function for sending a simple message.
+
+
+
+    def idle_proc(self):
+        """This function will be called in the main loop."""
+        self._idle_ping()
+
+    def _idle_ping(self):
+        """Pings the server, calls on_ping_timeout() on no response.
+
+        To enable set self.PING_FREQUENCY to a value higher than zero.
+        """
+        if self.PING_FREQUENCY \
+            and time.time() - self.__lastping > self.PING_FREQUENCY:
+            self.__lastping = time.time()
+            #logging.debug('Pinging the server.')
+            ping = xmpp.Protocol('iq', typ='get', \
+                payload=[xmpp.Node('ping', attrs={'xmlns':'urn:xmpp:ping'})])
+            try:
+                res = self.conn.SendAndWaitForResponse(ping, self.PING_TIMEOUT)
+                #logging.debug('Got response: ' + str(res))
+                if res is None:
+                    self.on_ping_timeout()
+            except IOError, e:
+                logging.error('Error pinging the server: %s, '\
+                    'treating as ping timeout.' % e)
+                self.on_ping_timeout()
+    
+    def on_ping_timeout(self):
+        logging.info('Terminating due to PING timeout.')
+        self.quit()
+
+    def shutdown(self):
+        """
+        This function will be called when we're done serving
+
+        Override this method in derived class if you
+        want to do anything special at shutdown.
+        """
+        pass
+
+
+    def serve_forever(self):
+        """
+        Connects to the server and handles messages.
+        """
+        conn = self.connect()
+        if conn:
+            self.log.info('Bot connected for eternal servitude.')
+        else:
+            self.log.warn('Could not connect to server - aborting')
+            return
+
+        while not self.__finished:
+            try: 
+                conn.Process(1)
+                self.idle_proc()
+            except KeyboardInterrupt:
+                self.log.info('Bot stopped by user request. '\
+                        'Shutting down.')
+                break
+
+
+
+
+
 
