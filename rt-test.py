e15ba771913492720bba3a6768fe91afe5690970
Geir Ulvik <ulvik@usit.uio.no>
2018-02-20 12:47:27 +0100

    Forgot to push

--- rt-test.py
+++ rt-test.py
@@ -1,26 +1,27 @@
 """
 Fetches relevant information from Request Tracker using
-the rt API.
-
-Tracker example:
-
-    import rt
-    tracker = rt.Rt('http://localhost/rt/REST/1.0/', 'user_login', 'user_pass')
-    tracker.login()
-    True
-    tracker.logout()
+the RT API.
 """
 
-import rt
 import getpass
+import urllib.request
+import urllib.parse
 
+import requests
 
 
 uname = getpass.getpass("rt-user:")
 upw = getpass.getpass("rt-pw: ")
 
-tracker = rt.Rt('http://rt.uio.no/REST/1.0/', uname, upw)
-tracker.login()
-print(tracker.login())
+# here is the RequestTracker URI we try to access
+base_url = 'http://rt.uio.no/REST/1.0/'
+
+r = requests.get(base_url + '?user=' + uname +'&pass=' + upw)
+print(r.status_code)
+print(r.text)
+jar = r.cookies
+a = requests.get(base_url, cookies=jar)
+print(a.text)
+
 
 
