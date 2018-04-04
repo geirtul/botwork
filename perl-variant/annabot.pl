de3643cdcb0b356d4a3a443b11040e6c737e524c
Geir Tore Ulvik <ulvik@saruman.uio.no>
2018-04-04 14:45:59 +0200

    Added beginning of example code to file. Need syntax training anyway.

--- perl-variant/annabot.pl
+++ perl-variant/annabot.pl
@@ -1,6 +1,15 @@
 #!/usr/bin/perl
-use strict;
-use warnings;
 
-print "Not implemented.\n";
+# ChatBot for houston based on: 
+# https://codehacienda.wordpress.com/2011/02/25/perl-xmpp-library-for-creating-a-chat-bot-anyeventxmpp-example/
+
+
+use utf8;
+use AnyEvent;
+use AnyEvent::XMPP::IM::Connection;
+use AnyEvent::XMPP::IM::Presence;
+use AnyEvent::XMPP::Util qw/split_jid/;
+ 
+my $j = AnyEvent->condvar;
+
 
